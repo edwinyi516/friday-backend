c67835ec65b54d938da63debf96a32de779c4cb6
Edwin <edwin.yi.128@gmail.com>
2022-10-07 20:16:43 -0500

    CORS opened

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ require("./config/db.connection");
 
 /* == Middleware == */
 app.use(cors({
-  origin: "https://friday-project-mgmt-frontend.herokuapp.com",
+  origin: "*",
   credentials: true
 }));
 app.use(express.json());
